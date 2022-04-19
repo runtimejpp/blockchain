1db68f662e78c30d217e79d57b861e6b41cc029f
Jay Pren <37848207+runtimejpp@users.noreply.github.com>
2022-04-19 17:17:03 -0400

    Update blockchain.js

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -34,9 +34,21 @@ Blockchain.prototype.hashBlock = function(previousBlockHash,currentBlockData,non
 
   const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
   const hash = sha256(dataAsString); //
- // return hash;
-  console.log(hash)
+  return hash;
+  
 
+};
+Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData){
+  let nonce = 0;
+  let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+  while (hash.substring(0,4) !== '0000'){
+    
+    nonce++
+    hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    console.log(hash);
+  }
+  return nonce;
+ 
 };
 
 
